177b93c79c814f5f2549e51c53304260fde07fa6
mrdp9 <145275154+mrdp9@users.noreply.github.com>
2026-05-26 11:13:21 +0000

    Add tests folder

--- app_config_service/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Application Configuration Service (ACS)
-A configuration management service that allows teams to manage 
-application configurations across different environments centrally.
-"""
-
-__version__ = "0.1.0"
-__author__ = "Mridul"
